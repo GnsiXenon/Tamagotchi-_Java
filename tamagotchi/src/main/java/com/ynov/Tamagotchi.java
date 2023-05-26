08ce273f8f3b885063a84f316b14ae1dee383db7
kylian oger <kylianoger@orange.fr>
2023-05-26 08:29:31 +0200

    add Principal Variable

--- tamagotchi/src/main/java/com/ynov/Tamagotchi.java
+++ tamagotchi/src/main/java/com/ynov/Tamagotchi.java
@@ -1,6 +1,42 @@
 package com.ynov;
 
+
+
 public class Tamagotchi {
+
+    /**
+     * Bonheur allant de 0 à 50
+     * @arg Bonheur : Niveau de bonheur du tamagotchi
+     */
+    Integer Bonheur = 15;
+    String Nom = "";
+    /**
+     * @arg Faim : False si le tamagotchi n'a pas été nourri sur l'unité de temps
+    */
+    Boolean Faim = false;
+    /**
+     * 
+     * @arg JoursSansManger : Nombre de jours sans manger
+     */
+    Integer JoursSansManger = 0;
+        /**
+     * @arg Propreté : Diminue de 3 le bonheur si le tamagotchi est sale
+     */
+    Boolean Propreté = true;
+    /**
+     * @arg Malade : 1/3 de chance d'être malade quand il est vieux
+     */
+    Boolean Malade = false;
+    /**
+     * @arg TimePlayed : nombre de fois jouer sur une unité de temps (0 à 3)
+     */
+    Integer TimePlayed = 0;
+    /**
+     * @arg Time : Temps de jeu en secondes
+     */
+    Integer Time = 30;
+    
+
 
 }
 
